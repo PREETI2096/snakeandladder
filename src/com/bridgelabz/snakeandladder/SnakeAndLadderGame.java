6d9284514f2bdd83f1a3c37f734fb1e7cca7b375
PREETI2096 <preetirg96@gmail.com>
2023-01-05 00:07:57 +0530

    [Preeti]: added snake and ladder program

--- src/com/bridgelabz/snakeandladder/SnakeAndLadderGame.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadderGame.java
@@ -1,16 +1,35 @@
 package com.bridgelabz.snakeandladder;
-
 import java.util.Random;
-
 public class SnakeAndLadderGame {
 
 	public static void main(String[] args) {
-		
-		 System.out.println("Welcome to snake and ladder game program");
+		System.out.println("Welcome to snake and ladder game program");
 		 int position = 0;
 		 System.out.println("position of single player is :" + position);
 		 Random random = new Random();
 		 int roll = 1 + random.nextInt(6);
 		 System.out.println("after rolling dice position is:" + roll);
+		 
+		 int options = random.nextInt(3);
+		 System.out.println("Option :" + options);
+        
+        switch (options) {
+        case 0:
+     	   System.out.println("Ladder i.e. move ahead!");
+     	   position += roll ;
+     	   break;
+     	   
+        case 1:
+     	   System.out.println("snake i.e. please move back");
+     	   position -= roll;
+     	   if(position < 0)
+     		   position = 0;
+     	   break;
+     	   
+        default:
+     	   System.out.println("No Play");
+        }
+        
+        System.out.println("position of single player is = " + position);       
 	}
 }
