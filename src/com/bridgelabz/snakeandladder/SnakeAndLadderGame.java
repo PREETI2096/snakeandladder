9d28ff0873774da010303c1e964741b94123225c
PREETI2096 <108446384+PREETI2096@users.noreply.github.com>
2023-01-10 14:57:31 +0530

    Added : snake and ladder program

--- src/com/bridgelabz/snakeandladder/SnakeAndLadderGame.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadderGame.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.snakeandladder;
 import java.util.Random;
 public class SnakeAndLadderGame {
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game program");
 		 int position = 0;
